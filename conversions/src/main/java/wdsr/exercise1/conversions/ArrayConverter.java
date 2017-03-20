3e630611ace6cbd53b60aeb75d0045e010343324
Paweł Siciński <koryfeusz.ksiezyca@gmail.com>
2017-03-20 11:55:17 +0100

    Exercise results. Dependencies, Log4J properties, logger use.

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,26 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
-public class ArrayConverter {
-    public int[] convertToInts(String[] strings) {
-        return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
-    }
-}
+public class ArrayConverter 
+	{
+	private static final Logger log = LogManager.getLogger();
+	
+	//____________________________________________________________________________________________________
+	
+    public int[] convertToInts( String [] strings ) {
+    	String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+    	log.debug( methodName + "; strings={};", Arrays.toString(strings) );
+        int [] result = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+        log.debug( methodName + " exiting; result={};", Arrays.toString(result) );
+        return result;
+    	}
+    
+    //____________________________________________________________________________________________________
+    
+	}
